1d15c0a6ed188e942fde7d642ac77c7a12d711f3
GatroxM <gamunoz@distry.co>
2020-01-20 09:44:20 -0500

    Se ajusta el app router+

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { PanelComponent } from './pages/panel/panel.component';
-import { RadicadosComponent } from './components/radicados/radicados.component';
 
 const routes: Routes = [
     { path: '', component: LoginComponent },
