31992b76e250a0be19af1f0e0ace8427459ae4bf
Gatroxm <tavoxpau@gmail.com>
2020-01-20 06:21:40 -0500

    Se muestra información y se crean las seccopnes

--- src/app/pages/panel/panel.component.ts
+++ src/app/pages/panel/panel.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class PanelComponent implements OnInit {
-
-  constructor() { }
+  consultas: any;
+  estado: boolean = false;
+  data = [
+    {
+      'nombre': 'Consulta médica',
+      'fecha': '24/01/2019',
+      'usuario': 'Alejandro amalla',
+      'serial': '6546518165321811-a',
+      'estados': [
+        {
+          'estado': 'creación de radicado',
+          'fecha': '24/01/2019',
+        },
+        {
+          'estado': 'trsaferencia de radicado',
+          'fecha': '25/01/2019',
+        },
+        {
+          'estado': 'En proceso de aprobación',
+          'fecha': '26/01/2019',
+        }
+      ],
+      'archivo': ''
+    }
+  ];
 
   ngOnInit() {
+    if (this.data) {
+      this.estado = true;
+      this.consultas = this.data;
+  console.log(data[0].estados)
+
+    }
+
   }
 
 }
